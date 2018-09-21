a8e417ec5d0736fded68372fa3c33b4177412317
HuiDi Chen <jjj96178@163.com>
2018-09-21 13:07:59 +0800

    Add files via upload

--- FastGCN.py
+++ FastGCN.py
@@ -12,12 +12,13 @@ dataset = data_processor()
 #data_processor.write_graphML(write_graphML_path=write_graphML_path)
 #data_processor.write_gexf(write_gexf_path=write_gexf_path)
 inputs,labels = data_processor.get_data()
-adjacent_matrix = np.array(data_processor.get_adjacent_matrix(),dtype="float32")[:50,:50]
+adjacent_matrix = np.array(data_processor.get_adjacent_matrix(),dtype="float32")
 
 placeholders = {}
 placeholders.update({"support":adjacent_matrix})
 
-model = fg.Sequential(name="fg",placeholders=placeholders,sample_num=20)
+model = fg.Sequential(name="fg",placeholders=placeholders,sample_num=100)
+model.add(fg.Dense(input_shape=inputs.shape,placeholders=placeholders,output_dim=inputs.shape[1]))
 model.add(fg.GraphConvolution(input_shape=inputs.shape,support=None,placeholders=placeholders,output_dim=[7],dropout=0.))
 model.compile(loss="masked_softmax_cross_entropy",metrics=["accuracy"])
-model.fit(x=inputs[0:50,:],y=labels[0:50,:],epochs=50,batch_size=50,sample_num=20)
+model.fit(x=inputs[:,:],y=labels[:,:],epochs=50,batch_size=100)
